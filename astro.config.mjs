91e77aa8719ec1c1fec738560d07043cb018c662
SteffenLehmann <Steffenlehmanndk@gmail.com>
2024-08-27 15:25:49 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://SteffenLehmann.github.io',
-  //base: '/Portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
